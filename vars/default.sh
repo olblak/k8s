895bba44218cbe2ebccb049bfbbfb7eeeaba9c41
Olivier Vernin <olivier@vernin.me>
2017-02-17 14:42:58 +0100

    Delete vars/default.sh

--- vars/default.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# All variables define in this script must respect following format
-# Which ensure that variable will only be if the value = null or doesn't exist
-#: "${VARIABLE}:=value"
-
-: "${ENV:=dev}"
-: "${SECRETS_PATH:=./env/$ENV/secrets}"
-: "${LOCATION:=eastus}"
-: "${PREFIX:=jenkinsinfra}"
