96f7dd921cfc5a72a5c3df260f480ea346814c89
Olivier Vernin <olivier@vernin.me>
2017-02-22 16:53:04 +0100

    Reload pods when configmap/secret changed

--- scripts/apply_resources.sh
+++ scripts/apply_resources.sh
@@ -4,19 +4,12 @@
 source ./scripts/helpers.sh
 
 
-# Delete pods with the same app value than daemonsets that was recently updated
+# Delete pods with the same app value provided by arguments
 function delete_pod {
-    for POD in $(kubectl get pods -l app="$1" -o name); do
+    for POD in $(kubectl get pods -l "$1" -o name); do
         kubectl delete "$POD"
     done
     echo "New $POD pods will be recreated soon by daemonset"
-}
-
-# Delete pods with the same app value than daemonsets that was recently updated
-function restart_pod {
-    for POD in $(kubectl get pods -l app="$1" -o name); do
-        kubectl restart "$POD"
-    done
 }
 
 # Apply two times daemonset configurations.
@@ -25,13 +18,14 @@ function restart_pod {
 # Daemonsets will then recreate them with correct configurations
 function apply_daemonset {
     DAEMON_SET=$1
-    NAME=$(kubectl describe -f "$DAEMON_SET" | grep 'Name:' | awk '{print $2}')
+    NAME=$(kubectl describe -f "$DAEMON_SET" | grep 'Name:' | awk '{print $2}' 2>/dev/null)
+    NAMESPACE=$(kubectl describe -f "$DAEMON_SET" | grep 'Namespace:' | awk '{print $2}' 2>/dev/null)
     GENERATION=$(kubectl get daemonset "$NAME" -o jsonpath="{.metadata.generation}")
     kubectl apply -f "$DAEMON_SET"
     NEW_GENERATION=$(kubectl get daemonset "$NAME" -o jsonpath="{.metadata.generation}")
     if [[ "$GENERATION" != "$NEW_GENERATION" ]];then
-        echo "$DAEMON_SET changed: $NAME  pods need to be reload"
-        delete_pod "$NAME"
+        echo "$DAEMON_SET changed: $NAME  pods need to be recreated"
+        delete_pod "app=$NAME"
     fi
 }
 
@@ -75,6 +69,22 @@ function check_configurations_files_nbr {
     fi
 }
 
+# Check if secret or configmap changed, if so restart all pods with label app=$NAME
+function apply_config {
+    CONFIG=$1
+    NAME=$(kubectl describe -f "$CONFIG" | grep 'Name:' | awk '{print $2}' 2>/dev/null)
+    NAMESPACE=$(kubectl describe -f "$CONFIG" | grep 'Namespace:' | awk '{print $2}' 2>/dev/null)
+    FILE=$(basename "$CONFIG")
+    KIND=${FILE/.yaml/}
+    GENERATION=$(kubectl get "$KIND" "$NAME" --namespace "$NAMESPACE" -o jsonpath="{.metadata.resourceVersion}" 2>/dev/null)
+    kubectl apply -f "$CONFIG"
+    NEW_GENERATION=$(kubectl get "$KIND" "$NAME" --namespace "$NAMESPACE" -o jsonpath="{.metadata.resourceVersion}")
+    if [[ "$GENERATION" != "$NEW_GENERATION" ]];then
+        echo "$NAME changed: $NAME  pods need to be restarted"
+        delete_pod "$KIND-$NAME=linked"
+    fi
+}
+
 # Browse DEFINITIONS_PATH for kubernetes configurations files 
 # Based on resource kind, we may have to apply different workflows
 function run {
@@ -82,6 +92,9 @@ function run {
         case "$(basename "$file")" in 
             "daemonset.yaml") 
                 apply_daemonset "$file"
+                ;; 
+            "secret.yaml"|"configmap.yaml") 
+                apply_config "$file"
                 ;; 
             *) 
                 apply_resource "$file"
