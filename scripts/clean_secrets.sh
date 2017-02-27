8094cb6bed9bfb7b7291fcb04d3e7f31abb3b1d5
Olivier Vernin <olivier@vernin.me>
2017-02-27 17:06:25 +0100

    add delete secret.yaml to clean_secrets.sh

--- scripts/clean_secrets.sh
+++ scripts/clean_secrets.sh
@@ -6,4 +6,5 @@ set +e
 source ./scripts/helpers.sh
 
 find "$CONFIGURATIONS_PATH" -name 'secrets.yaml' -delete
+find "$CONFIGURATIONS_PATH" -name 'secret.yaml' -delete
 find "$CONFIGURATIONS_PATH" -name '*.yaml.generated' -delete
