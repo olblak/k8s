fe89391b925e7d34663c75b7196695c09fec470f
Olivier Vernin <olivier@vernin.me>
2017-02-27 16:51:35 +0100

    Only update vault file when change were applied
    	Up to now each time we generating secrets, we were
    	overidding vault files.

--- scripts/encrypt_secrets.sh
+++ scripts/encrypt_secrets.sh
@@ -6,6 +6,18 @@ set +e
 source ./scripts/helpers.sh
 
 while read -r file; do 
+    # Compare newly generated secret file with vault content to only update vault file if needed
+    if [ -f "${file/yaml.generated/gpg}" ]; then
+        gpg --passphrase "$VAULT_PASSWORD" --yes -d --output "${file/generated/tmp}" "${file/yaml.generated/gpg}"
+
+        if [[ "$(diff -I '^#' "${file/generated/tmp}" "${file}")" == '' ]]; then
+            rm "${file/generated/tmp}"
+            continue
+        else
+            rm "${file/generated/tmp}"
+            echo "${file/yaml.generated/gpg}: Updated"
+        fi
+    fi
     gpg --passphrase "$VAULT_PASSWORD" --yes -c --output "${file/yaml.generated/gpg}" "$file"
     rm "$file"
 done < <(find "$CONFIGURATIONS_PATH" -name '*.yaml.generated') 
